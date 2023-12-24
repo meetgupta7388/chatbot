9c1a82c394dbe2317a24cb38fb54e8df7a99c2e3
meetgupta7388 <137211709+meetgupta7388@users.noreply.github.com>
2023-12-25 00:20:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,33 +1,43 @@
-from flask import Flask,request,jsonify
 import requests
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route('/',methods=['POST'])
+@app.route('/', methods=['POST'])
 def index():
     data = request.get_json()
-    source_currency = data['queryResult']['parameters']['unit-currency']['currency']
+    source_currency = data['queryResult']['parameters']['unit-currency']['currency'] 
     amount = data['queryResult']['parameters']['unit-currency']['amount']
-    target_currency = data['queryResult']['parameters']['currency-name']
+    
+    # Get just string without quotes 
+    target_currency = data['queryResult']['parameters']['currency-name'][0]
 
-
-    cf = fetch_conversion_factor(source_currency,target_currency)
+    cf = fetch_conversion_factor(source_currency, target_currency)
+    
+    if cf is None:
+        response = {
+            "fulfillmentText": "Sorry, unable to get conversion rate."
+        }
+        return jsonify(response)
+        
     final_amount = amount * cf
     final_amount = round(final_amount,2)
+    
     response = {
-        'fulfillmentText':"{} {} is {} {}".format(amount,source_currency,final_amount,target_currency)
+        "fulfillmentText": "{} {} is {} {}".format(amount, source_currency, 
+                                                   final_amount, target_currency)
     }
+    
     return jsonify(response)
 
-def fetch_conversion_factor(source,target):
-
-    url = "https://free.currconv.com/api/v7/convert?q={}_{}&compact=ultra&apiKey=af9fe87465f485e7013a".format(source,target)
-
+def fetch_conversion_factor(source, target):
+    url = "https://free.currconv.com/api/v7/convert?q={}_{}&compact=ultra&apiKey=af9fe87465f485e7013a".format(source, target)
     response = requests.get(url)
-    response = response.json()
-
-    return response['{}_{}'.format(source,target)]
-
+    
+    try:
+        return response.json()['{}_{}'.format(source, target)]
+    except KeyError: 
+        return None
 
 if __name__ == "__main__":
     app.run(debug=True)
